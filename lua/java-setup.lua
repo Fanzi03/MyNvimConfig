7c7a036b915742e990e4360037e0b0c43b834965
fanzi03 <tirolrol15@gmail.com>
2025-06-06 16:44:49 +0800

    +tests

--- lua/java-setup.lua
+++ lua/java-setup.lua
@@ -22,14 +22,43 @@ function M.setup()
             "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
             "-configuration", home .. "/.local/share/nvim/mason/packages/jdtls/config_linux",
             "-data", workspace_dir
-        "-javaagent:" .. home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
         },
-        root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew" }),
+        test_command = {
+            ["junit"] = {
+                "java",
+                "-cp",
+                "${classpath}",
+                "org.junit.jupiter.JUnitPlatform",
+                "--classpath",
+                "${classpath}"
+            },
+            ["mocha"] = {
+                "java",
+                "-cp",
+                "${classpath}",
+                "org.mortbay.jetty.Main",
+                "--reporter", "junitxml",
+                "--test", "mocha"
+            },
+            ["jest"] = {
+                "java",
+                "-cp",
+                "${classpath}",
+                "jest",
+                "--runTestsInParallel"
+            }
+
+        },
+        root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "test" }),
     }
 
     jdtls.start_or_attach(config)
     require('jdtls').setup_dap({ hotcodereplace = 'auto' })
     require('jdtls.dap').setup_dap_main_class_configs()
+    require('jdtls.test').setup_test()
+
+
+    -- Add this line to run your tests
 end
 
 return M
